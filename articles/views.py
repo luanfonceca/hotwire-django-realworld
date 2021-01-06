c2e659ac41d512628556a4920f70d678f2e1163a
Christopher Grande <chrisgrande@mac.com>
2021-01-05 22:49:15 -0500

    Base Detail view wiring

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
+from .models import Article
 
 
 def view(req, slug):
-    return render(req, "articles/detail.html")
+    article = get_object_or_404(Article, slug=slug)
+    return render(req, "articles/detail.html",
+                            {'article': article})
 
 
 @login_required
